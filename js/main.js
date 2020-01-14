c1273a431b61f6b60d085098eaf739a12d46372c
banobepascal <banobepascal@gmail.com>
2020-01-14 22:57:45 +0300

    ft(adding styles):
    - added colors and gradients to layout
    - modified index.html and removed unnecessary lines
    - refactored style.css
    - modified main.js
    
    Finishes

--- js/main.js
+++ js/main.js
@@ -15,27 +15,6 @@ $(document).ready(function() {
 
   $(".project-area .button-group #btn1").trigger("click");
 
-//   $(".project-area .grid .test-popup-link").magnificPopup({
-//     type: "image",
-//     gallery: { enabled: true }
-//   });
-
-  // Owl-carousel
-
-  // $('.site-main .about-area .owl-carousel').owlCarousel({
-  //     loop: true,
-  //     autoplay: true,
-  //     dots: true,
-  //     responsive: {
-  //         0: {
-  //             items: 1
-  //         },
-  //         560: {
-  //             items: 2
-  //         }
-  //     }
-  // })
-
   // sticky navigation menu
 
   let nav_offset_top = $(".header_area").height() + 50;
@@ -47,7 +26,7 @@ $(document).ready(function() {
         if (scroll >= nav_offset_top) {
           $(".header_area .main-menu").addClass("navbar_fixed nav_color");
         } else {
-          $(".header_area .main-menu").removeClass("navbar_fixed");
+          $(".header_area .main-menu").removeClass("navbar_fixed nav_color");
         }
       });
     }
@@ -55,3 +34,4 @@ $(document).ready(function() {
 
   navbarFixed();
 });
+
