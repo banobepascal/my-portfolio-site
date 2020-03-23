6bca38dcc5eed36dc11c7ed5c04a4d5716da51f7
banobepascal <banobepascal@gmail.com>
2020-03-23 16:33:29 +0300

    ch(setting up app)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import Homepage from './Pages/Homepage/Homepage.component'
+import {AboutTag, WorkTag, ContactTag, FooterContainer} from './App.styles'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <AboutTag>about</AboutTag>
+    <WorkTag>work</WorkTag>
+    <ContactTag>contact</ContactTag>
+      <Homepage/>
+    <FooterContainer>
+      <a href="#">Banobe Pascal</a>
+    </FooterContainer>
     </div>
   );
 }
