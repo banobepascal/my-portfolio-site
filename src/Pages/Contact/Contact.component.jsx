6d4e584ffadafacabd3cc39e1afa9a23ba8493a0
banobepascal <banobepascal@gmail.com>
2020-03-24 22:03:37 +0300

    ft(adding page transition on contact component with jquery )

--- src/Pages/Contact/Contact.component.jsx
+++ src/Pages/Contact/Contact.component.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import $ from 'jquery';
 
 import {Container, Headings, BackPointer} from '../container.styles';
 import {FormInput, FormTextArea, SendButton} from './Contact.styles';
 
 const ContactPage = () => {
+  const closeContact = () => {
+    $(".contact_container").addClass("animated slideOutDown");
+    setTimeout(function(){
+        $(".contact_container").removeClass("animated slideOutDown");
+        $(".contact_container").css("display","none");
+    },800);
+  }
+
   return (
-    <Container>
-      <BackPointer>
+    <Container className='contact_container'>
+      <BackPointer onClick={closeContact}>
         <i className="fas fa-angle-down"></i>
       </BackPointer>
       <Headings>contact.</Headings>
